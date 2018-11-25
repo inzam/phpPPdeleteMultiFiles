5579a61f94f5d8fb9dbdd7aa454669a7eefd0821
Inzam Zaman <inzam.zaman@inzam.com>
2018-11-25 09:39:18 +0600

    form to take input and delete

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!--delete mutliple file-->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Delete files</title>
+</head>
+<body>
+
+<form action="delete.php" method="post">
+    <input type="text" name="filename" style="width: 300px">
+    <button type="submit" name="delete">Delete</button>
+</form>
+
+</body>
+</html>
